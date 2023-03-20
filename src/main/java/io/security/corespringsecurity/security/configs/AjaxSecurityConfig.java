0166dc69e8a5ceadc263deee2a21d3381b7d61e2
jangwon3828 <wkd3828@gmail.com>
2023-03-20 13:47:25 +0900

    5-5) 인증 및 인가 예외 처리 - AjaxLoginUrlAuthenticationEntryPoint, AjaxAccessDeniedHandler

--- src/main/java/io/security/corespringsecurity/security/configs/AjaxSecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/AjaxSecurityConfig.java
@@ -1,19 +1,19 @@
 package io.security.corespringsecurity.security.configs;
 
+import io.security.corespringsecurity.security.common.AjaxLoginAuthenticationEntryPoint;
 import io.security.corespringsecurity.security.filter.AjaxLoginProcessingFilter;
+import io.security.corespringsecurity.security.handler.AjaxAccessDeniedHandler;
 import io.security.corespringsecurity.security.handler.AjaxAuthenticationFailureHandler;
 import io.security.corespringsecurity.security.handler.AjaxAuthenticationSuccessHandler;
-import io.security.corespringsecurity.security.provider.AjaxAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -61,9 +61,16 @@ public class AjaxSecurityConfig {
                 .anyRequest().authenticated();
         http    //UsernamePasswordAuthenticationFilter 앞에서 작동하게 설정
                 .addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.exceptionHandling()
+                        .authenticationEntryPoint(new AjaxLoginAuthenticationEntryPoint())
+                                .accessDeniedHandler(ajaxAccessDeniedHandler());
         http
                 .csrf().disable();
         return http.build();
+    }
+
+    private AccessDeniedHandler ajaxAccessDeniedHandler() {
+        return  new AjaxAccessDeniedHandler();
     }
 
 
